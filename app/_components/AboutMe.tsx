84413b69fcc184472fc4de0e654748674895314b
PedroDanesvara <danesvara2002@gmail.com>
2025-06-09 15:54:05 -0300

    chore: update About Me

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -54,37 +54,32 @@ const AboutMe = () => {
     return (
         <section className="pb-section" id="about-me">
             <div className="container" ref={container}>
-                <h2 className="text-4xl md:text-6xl font-thin mb-20 slide-up-and-fade">
-                    I believe in a user centered design approach, ensuring that
-                    every project I work on is tailored to meet the specific
-                    needs of its users.
+                <h2 className="text-4xl md:text-6xl font-thin mb-20 slide-up-and-fade text-center">
+                Acredito que a experiência do usuário deve ser o centro de qualquer projeto, 
+                guiando decisões em todas as camadas — do backend à interface — para que cada 
+                solução atenda de forma eficaz as necessidades reais dos usuários.
                 </h2>
 
                 <p className="pb-3 border-b text-muted-foreground slide-up-and-fade">
-                    This is me.
+                    Este sou eu.
                 </p>
 
                 <div className="grid md:grid-cols-12 mt-9">
                     <div className="md:col-span-5">
                         <p className="text-5xl slide-up-and-fade">
-                            Hi, I&apos;m Tajmirul.
+                            Olá, sou Pedro.
                         </p>
                     </div>
                     <div className="md:col-span-7">
                         <div className="text-lg text-muted-foreground max-w-[450px]">
                             <p className="slide-up-and-fade">
-                                I&apos;m a frontend web developer dedicated to
-                                turning ideas into creative solutions. I
-                                specialize in creating seamless and intuitive
-                                user experiences.
+                            Sou um desenvolvedor Full Stack com foco em criar soluções com foco no usuário. 
                             </p>
                             <p className="mt-3 slide-up-and-fade">
-                                My approach focuses on creating scalable,
-                                high-performing solutions tailored to both user
-                                needs and business objectives. By prioritizing
-                                performance, accessibility, and responsiveness,
-                                I strive to deliver experiences that not only
-                                engage users but also drive tangible results.
+                                Meu foco é criar soluções eficientes, escaláveis e 
+                                responsivas para atender às necessidades reais dos usuários e do 
+                                projeto. Foco em entregar experiências que não apenas envolvam os usuários, mas também 
+                                impulsionem resultados tangíveis.
                             </p>
                         </div>
                     </div>
